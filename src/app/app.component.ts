02b268c37454f512f4aac174aa2d697b13fb858b
nagz <nagababu.thota111@gmail.com>
2021-04-30 16:05:09 +0530

    dev updated to merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'practice';
+  title = 'practice to update dev';
   title2 = "merging dev branch into main";
 }
